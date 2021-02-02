6e8926697c340bd70fad9aebc2cd621b982e12b6
runan13 <65391973+runan13@users.noreply.github.com>
2021-02-02 20:09:31 +0900

    2.1 Create Account #2

---
+++ users/users.mutations.js
@@ -0,0 +1,27 @@
+import client from "../client";
+
+export default {
+  Mutation: {
+    createAccount: async (
+      _,
+      { firstName, lastName, username, email, password }
+    ) => {
+      // Check username or email are already on DB
+      const existingUser = await client.user.findFirst({
+        where: {
+          OR: [
+            {
+              username,
+            },
+            {
+              email,
+            },
+          ],
+        },
+      });
+      console.log(existingUser);
+      // Hash Password
+      // save and return the user
+    },
+  },
+};
