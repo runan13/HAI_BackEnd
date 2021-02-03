17a55b64d0b0c83c31e3e21b8769433ef40fe480
runan13 <65391973+runan13@users.noreply.github.com>
2021-02-03 15:17:31 +0900

    2.6 authentication #2

--- server.js
+++ server.js
@@ -6,6 +6,10 @@ const PORT = process.env.PORT;
 
 const server = new ApolloServer({
   schema,
+  context: {
+    token:
+      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6NCwiaWF0IjoxNjEyMzMyMDQxfQ.NnlCo5lzB_Kf76xTNmDdhisbwxY9-SmWxps61bStqi8",
+  },
 });
 
 server
