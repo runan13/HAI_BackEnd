e598484af3167a51cd65dc43c82680f0e7f6608c
runan13 <65391973+runan13@users.noreply.github.com>
2021-02-08 15:04:41 +0900

    2.9 Ejecting from Apollo Server

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 require("dotenv").config();
-import { ApolloServer } from "apollo-server";
+import express from "express";
+import logger from "morgan";
+import { ApolloServer } from "apollo-server-express";
 import { typeDefs, resolvers } from "./schema";
 import { getUser, protectResolver } from "./users/users.utility";
 
@@ -16,8 +18,9 @@ const server = new ApolloServer({
   },
 });
 
-server
-  .listen(PORT)
-  .then(() =>
-    console.log(` ✅ Server is Running on http://localhost:${PORT}/`)
-  );
+const app = express();
+app.use(logger("tiny"));
+server.applyMiddleware({ app });
+app.listen({ port: PORT }, () => {
+  console.log(` ✅ Server is Running on http://localhost:${PORT}/`);
+});
